06cc72b2092d34dc3af835d657d6c3adf15e5931
chelseashin <chaewonshin95@gmail.com>
2019-05-17 02:44:26 +0000

    190517 | last commit

--- final_project/wsgi.py
+++ final_project/wsgi.py
@@ -7,17 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'final_project.settings')
-
-# application = get_wsgi_application()
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kloudless.settings")
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'final_project.settings')
+
+application = get_wsgi_application()
+# import os
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kloudless.settings")
+# from django.core.wsgi import get_wsgi_application
+# from dj_static import Cling
+
+# application = Cling(get_wsgi_application())
